17b6ba9baabe56a7f42cb5c08b219496dbb782e8
JacobGower <jgower@student.rccd.edu>
2021-03-08 18:55:37 -0800

    Signed-off-by: JacobGower <jgower@student.rccd.edu>

--- Hmwk/Gaddis_9thEd_Chap4_Prob10_DaysInAMonth/main.cpp
+++ Hmwk/Gaddis_9thEd_Chap4_Prob10_DaysInAMonth/main.cpp
@@ -22,12 +22,48 @@ int main(int argc, char** argv) {
     //Initialize the Random Number Seed
     
     //Declare Variables
+    short   month,  //Month number, input by User
+             year;  //Year number, input by User
+    char  address;  //For referencing the address in the days array
+                //Number of days in each month 1-12. 29 at index 0 for leap year
+    const char days[]={29,31,28,31,30,31,30,31,31,30,31,30,31};
     
     //Initialize Variables
+    month=year=0;
+    
+    while (month<1||month>12) { //Input Validation for months
+        cout<<"Enter a month (1-12): ";
+        cin>>month;
+        cout<<endl;
+        
+        if (month<1||month>12) { //Invalid input Message
+           cout<<"You must not be using any calendar I've ever heard of."<<endl;
+           cout<<"Please enter a number between 1 and 12"<<endl;
+        }
+        
+    }
+    //Technically some BC values are Valid, but for simplicity, limit to AD (>0)
+    while (year<1) { //Input Validation for years
+        cout<<"Enter a year (AD only): ";
+        cin>>year;
+        cout<<endl;
+        if (year<1) { //Invalid input Message
+            cout<<"Please enter a valid year."<<endl;
+        }
+    }
     
     //Map Inputs to Outputs -> Process
+    if (month==2) { //Test for Leap Year 
+        address=(year%100==0&&year%400==0?0:
+                 year%100!=0&&year%4==0?0:2);
+                   
+    }else {
+        address=month;
+    }
 
     //Display Inputs/Outputs
+    //Static cast days address value to an int to avoid displaying ASCII value
+    cout<<static_cast<int>(days[address])<<" days";
       
     //Exit the Program - Cleanup
     return 0;
--- Hmwk/Gaddis_9thEd_Chap5_Prob11_Population/main.cpp
+++ Hmwk/Gaddis_9thEd_Chap5_Prob11_Population/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Jacob Gower
  * Created on March 7, 2021, 10:45 AM
- * Purpose:  To Predict the Size of a Population of Organisms
+ * Purpose:  To Predict the Size of a Population of Organisms over time
  */
 
 //System Libraries
@@ -21,12 +21,61 @@ int main(int argc, char** argv) {
     //Initialize the Random Number Seed
     
     //Declare Variables
-    
+    int    begPop, //Beginning Population of Organisms
+             days, //Number of days the population increases
+           finPop; //Final Population of Organisms
+    float dayPrct; //Average daily Population Increase as a Percent
+            
     //Initialize Variables
+    begPop=days=dayPrct=0;
+    
+    while (begPop<2) {
+        cout<<"Input starting number of organisms (Minimum of 2):";
+        cin>>begPop;
+        cout<<endl;
+        
+        if (begPop<2) {
+            cout<<"I think I need to tell you about the birds and the bees.\n";
+        }
+    }
+    
+    finPop=begPop;
+    
+    do {
+        cout<<"Please input the average daily population ";
+        cout<<"increase as a percentage:";
+        cin>>dayPrct;
+        cout<<endl;
+        
+        if (dayPrct<0) {
+            cout<<"This calculates population growth, not reduction."<<endl;
+        }
+    }while (dayPrct<0);
+    
+    dayPrct/=100.0f;
+    
+    do {
+        cout<<"Please input the Number of days that the ";
+        cout<<"population increases: "<<endl;
+        cin>>days;
+        cout<<endl;
+        
+        if (days<1) {
+            cout<<"These things take more time than that."<<endl;
+        }
+    }while (days<1);
+    
+    
     
     //Map Inputs to Outputs -> Process
 
     //Display Inputs/Outputs
+    
+    for (int i=1;i<=days;i++) {
+       finPop+=finPop*dayPrct;
+       
+       cout<<"Population Day "<<i<<": "<<finPop<<endl;
+    }
       
     //Exit the Program - Cleanup
     return 0;
